021a17c5ea45455bdb03670962cc96104fa42af7
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2024-04-12 13:40:27 +0530

    Added Prod url

--- Frontend/medvault/src/helper/axiosInstance.js
+++ Frontend/medvault/src/helper/axiosInstance.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const BASE_URL="http://localhost:3000/api/v1";
-
+const PROD_URL="https://fullstack-medvault.onrender.com/api/v1"
 const axiosInstance=axios.create();
 
-axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.baseURL=PROD_URL;
 axiosInstance.defaults.withCredentials=true;
 
 
