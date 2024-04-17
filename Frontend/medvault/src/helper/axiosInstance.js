29dd25764c865e0296404dd2b305f0f60e832f79
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2024-04-17 16:55:18 +0530

    changed to production environment

--- Frontend/medvault/src/helper/axiosInstance.js
+++ Frontend/medvault/src/helper/axiosInstance.js
@@ -4,7 +4,7 @@ const BASE_URL="http://localhost:3000/api/v1";
 const PROD_URL="https://fullstack-medvault.onrender.com/api/v1"
 const axiosInstance=axios.create();
 
-axiosInstance.defaults.baseURL=BASE_URL;
+axiosInstance.defaults.baseURL=PROD_URL;
 axiosInstance.defaults.withCredentials=true;
 
 
